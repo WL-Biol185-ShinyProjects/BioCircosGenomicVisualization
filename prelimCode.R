81acf8af236f7f5b2878e3ae58f0ccadb9b7d162
herrerae20 <herrerae20@mail.wlu.edu>
2019-11-24 12:01:19 -0500

    lines 1-7, installation of BioCricos Lines 11-15, bare human genome BioCircos Lines 20-29, this is a cool feture that lets you hsee the copy number varinants on the gene backgroud we choose Lines 34-42, this is the general outline for the code for the arcs that represent the genes in the circle
    Lines 48-58, this is the general form of the backgroud that is the genes we want as well as the overlay of the SNP data, with labels for SNP mutations we want to see on the visial

---
+++ prelimCode.R
@@ -0,0 +1,67 @@
+#new pakage#
+
+install.packages('BioCircos')
+
+if (!require('devtools')){install.packages('devtools')}
+
+devtools::install_github('lvulliard/BioCircos.R', build_vignettes = TRUE)
+
+
+
+#get circle#
+
+library(BioCircos)
+
+BioCircos()
+
+
+
+
+BioCircos(
+  BioCircosCNVTrack('BarTrack', 
+                    chromosomes = 1:3, 
+                    starts = 1e+7*2:4, 
+                    ends = 2.5e+7*2:4,
+                    values = 1:3, 
+                    color = "#BB0000", 
+                    maxRadius = 0.85, 
+                    minRadius = 0.55) 
+  + BioCircosBackgroundTrack('BGTrack'))
+
+
+
+
+BioCircosArcTrack(trackname, 
+                  chromosomes, 
+                  starts, 
+                  ends,
+                  colors = "#40B9D4", 
+                  labels = "", 
+                  opacities = 1, 
+                  maxRadius = 0.9,
+                  minRadius = 0.5)
+
+
+
+
+
+BioCircos(
+  BioCircosSNPTrack('SNPTrack', 
+                    chromosomes = 1:3,
+                    positions = 1e+7*2:4, 
+                    values = 1:3, 
+                    colors = "Accent", 
+                    labels = c('A to G', 'B', 'C'))
+  + BioCircosArcTrack('ArcTrack', 
+                      chromosomes = 1:5, 
+                      starts = 2e+7*1:5, 
+                      ends = 2.5e+7*2:6))
+
+
+
+
+
+
+
+
+
