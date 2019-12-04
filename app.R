359dc79965a70bb1f888e3f98c77b3b0ea07e08c
Erin McFall <mcfalle20@mail.wlu.edu>
2019-12-04 13:56:05 -0500

    Expanded the basic shiny dashboard. Adjusted title and sidebar width to provide space for the long title.
    Added tabs to the sidebar with corresponding icons (lines 13-31). Added basic tab body content (lines 49-85).
    
    On the "BioCircos Plot" tab, I added a place for users to upload a csv file with their genetic information and began adding specifications to tell R what elements the uploaded file will contain.
    
    Next: will add logic in the server function for R to read the csv file.

--- app.R
+++ app.R
@@ -1,15 +1,93 @@
-library(shiny)
+install.packages("shinydashboard")
+
+## app.R ##
 library(shinydashboard)
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Genomic Visualization With BioCircos"),
-  dashboardSidebar(),
-  dashboardBody()
+  dashboardHeader(title = "Geonomic Visualization",
+                  titleWidth = 300
+                  ),
+  dashboardSidebar(
+    width = 300,
+    
+    # Add sidebar menu items (tabs) with corresponding icons 
+    # Icons are sourced from: https://fontawesome.com/icons?d=gallery
+    sidebarMenu(
+      menuItem("Home", 
+               tabName = "home", 
+               icon = icon("home")),
+      menuItem("BioCircos Plot",
+               tabName = "plot",
+               icon = icon("first-order-alt")),
+      menuItem("Interpreting Your Data",
+               tabName = "interpret",
+               icon = icon("dna")),
+      menuItem("Next Steps",
+               tabName = "nextSteps",
+               icon = icon("shoe-prints")),
+      menuItem("Documentation",
+               tabName = "documentation",
+               icon = icon("chart-line"))
+    )
+  ),
+  
+  dashboardBody(
+    tabItems(
+      # Home tab content
+      tabItem(tabName = "home",
+              
+              # Home tab body content
+              fluidRow(
+                box(
+                  title = "Welcome",
+                  status = "primary",
+                  h2("Homepage content")
+                )
+              )
+      ),
+      
+      # BioCircos Plot tab content
+      tabItem(tabName = "plot",
+              h2("Your BioCircos Plot"),
+              
+              #Input: upload a csv file of your gene snp's
+              fileInput("file1",
+                        "Choose a CSV file",
+                        multiple = FALSE,
+                        accept = c("text/csv",
+                                   "text/comma-separated-values,text/plain",
+                                   ".csv")
+                        ),
+              
+              # Aesthetic Horizontal line 
+              tags$hr(),
+              
+              # Creating check box for user to indicate
+              # if the csv input file has a header
+              checkboxInput("header", "Header", TRUE),
+              
+      ),
+      
+      # Interpreting Your Data tab content
+      tabItem(tabName = "interpret",
+              h2("Interpreting Your Data")
+      ),
+      
+      # Next Steps tab content
+      tabItem(tabName = "nextSteps",
+              h2("What are my next steps?")
+      ),
+      
+      # Documentation tab content
+      tabItem(tabName = "documentation",
+              h2("Documentation")
+      )
+    )
+  )
 )
 
-server <- function(input,output) {
-  
-  
-}
+## server ##
+server <- function(input, output) { }
 
-shinyApp(ui,server)
+## Display the app ##
+shinyApp(ui, server)
