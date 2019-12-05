d81b9dfbdadcf9e5bc8fa786a73cd054f56735d9
Erin McFall <mcfalle20@mail.wlu.edu>
2019-12-05 11:14:06 -0500

    Added homepage text content and citations on "Documentation Tab"

--- app.R
+++ app.R
@@ -1,6 +1,8 @@
+install.packages("shiny")
 install.packages("shinydashboard")
 
 ## app.R ##
+library(shiny)
 library(shinydashboard)
 
 ui <- dashboardPage(
@@ -41,7 +43,19 @@ ui <- dashboardPage(
                 box(
                   title = "Welcome",
                   status = "primary",
-                  h2("Homepage content")
+                  p("With advances in modern medicine and genomic sequencing, 
+                    we now have the opportunity to create specific treatment protocols for 
+                    patients who have not been successful in their search for effective medical 
+                    management. Genetic therapies hold promise for several pathologies such as 
+                    cystic fibrosis, cardiovascular disease, HIV, AIDS, and a variety of cancers. 
+                    Gene therapy is still in its infancy in the realm of clinical research, 
+                    but is predicted to become standard treatment in future medicine."),
+                  p("With this app, you can upload a file of patient genomic data in the form 
+                    of SNP’s and generate a BioCircos plot indicating the presence and location 
+                    of a variety of mutations with known disease associations. To start 
+                    generating your plot, read more on the “BioCircos Plot” tab. To understand 
+                    your graph, read “Interpreting Your Data”. To find out more about genetic 
+                    counselling or to explore your future treatment options, read “Next Steps.”")
                 )
               )
       ),
@@ -70,7 +84,9 @@ ui <- dashboardPage(
               
               # Creating check box for user to indicate
               # if the csv input file has a header
-              checkboxInput("header", "Header", TRUE),
+              checkboxInput("header", 
+                            "Header", 
+                            TRUE),
               
               # Adding radio buttons for user to select how
               # data in their csv file is separated. 
@@ -78,8 +94,8 @@ ui <- dashboardPage(
                            "Separator",
                            choices = c(Comma = ",",
                                        Semicolon = ";",
-                                       Tab = "\t",
-                                       None),
+                                       Tab = "\t"
+                                       ),
                            selected = ","),
               
               # Creating a main panel where user data file 
@@ -110,7 +126,17 @@ ui <- dashboardPage(
       tabItem(tabName = "documentation",
               
               # Page header text
-              h2("Documentation")
+              h2("Documentation"),
+              
+              box(
+                
+                # Page body text
+                p("Bloss CS, Jeste DV, Schork NJ. Genomics for disease treatment and prevention. 
+                  Psychiatr Clin North Am. 2011;34(1):147–166. doi:10.1016/j.psc.2010.11.005"),
+                p("Gonçalves GAR, Paiva RMA. Gene therapy: advances, challenges and perspectives.
+                  Einstein (Sao Paulo). 2017;15(3):369–375. doi:10.1590/S1679-45082017RB4024"),
+                p("McCain J. The future of gene therapy. Biotechnol Healthc. 2005;2(3):52–60.")
+              )
       )
     )
   )
@@ -132,4 +158,4 @@ server <- function(input, output) {
 }
 
 ## Display the app ##
-shinyApp(ui, server)
+shiny::shinyApp(ui = ui, server = server)
